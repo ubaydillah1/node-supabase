95b92043a33a39dea82c1ecdd728389ca2abc769
ubaydillah1 <ubaydillah1737@gmail.com>
2025-02-07 06:20:50 +0700

    run without db

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,18 @@ import express from "express";
 
 const app = express();
 
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
+// const { PrismaClient } = require("@prisma/client");
+// const prisma = new PrismaClient();
 
-app.get("/user", async (req, res) => {
-  const val = await prisma.user.findMany({
-    take: 10,
-  });
+// app.get("/user", async (req, res) => {
+//   const val = await prisma.user.findMany({
+//     take: 10,
+//   });
 
-  res.json({
-    user: val,
-  });
-});
+//   res.json({
+//     user: val,
+//   });
+// });
 
 app.get("*", (req, res) => {
   res.json({
@@ -25,20 +25,20 @@ const server = app.listen(3000, async () => {
   console.log("http://localhost:3000");
 });
 
-process.on("SIGINT", async () => {
-  console.log("Shutting down server...");
-  await prisma.$disconnect();
-  server.close(() => {
-    console.log("Server closed.");
-    process.exit(0);
-  });
-});
+// process.on("SIGINT", async () => {
+//   console.log("Shutting down server...");
+//   await prisma.$disconnect();
+//   server.close(() => {
+//     console.log("Server closed.");
+//     process.exit(0);
+//   });
+// });
 
-process.on("SIGTERM", async () => {
-  console.log("Shutting down server...");
-  await prisma.$disconnect();
-  server.close(() => {
-    console.log("Server closed.");
-    process.exit(0);
-  });
-});
+// process.on("SIGTERM", async () => {
+//   console.log("Shutting down server...");
+//   await prisma.$disconnect();
+//   server.close(() => {
+//     console.log("Server closed.");
+//     process.exit(0);
+//   });
+// });
